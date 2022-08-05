00984613fcaccb5931e6a07105fb917682aca698
Mr.Chen <1206084693@qq.com>
2022-08-05 17:07:28 +0800

    demo搭建

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '14.0'
+
+target 'YHDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for YHDemo
+#UI
+pod 'Hero'
+
+#Layout
+pod 'SnapKit'
+
+#Tools
+pod 'R.swift'
+
+
+
+
+end
---
+++ YHDemo/Modules/MovableGride/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  YHDemo
+//
+//  Created by 陈毅琥 on 2022/8/5.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
